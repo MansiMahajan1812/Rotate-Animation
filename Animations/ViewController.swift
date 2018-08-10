b8b8f2989ecbe166e371530b6d77e8ae52c67ef5
Mansi Mahajan <Mansi@Amrits-Mac-Mini.local>
2018-08-10 09:39:15 +0530

    first comment

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -15,6 +15,39 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+
+
+    @IBOutlet weak var btn: UIButton!
+    var degree = CGFloat(Double.pi)
+    @IBAction func Rotate(_ sender: UIButton) {
+      //    sectionIsExpanded = !sectionIsExpanded
+       
+        if degree == -2*CGFloat(Double.pi) {
+            UIView.animate(withDuration: 0.25) {
+                self.btn.transform = CGAffineTransform(rotationAngle: self.degree)
+            }
+            degree = CGFloat(Double.pi)
+        }else{
+            UIView.animate(withDuration: 0.25) {
+                self.btn.transform = CGAffineTransform(rotationAngle: self.degree)
+                
+            }
+            degree = -2*CGFloat(Double.pi)
+        }
+        print(self.degree)
+    }
+    
+//    var sectionIsExpanded: Bool = true {
+//        didSet {
+//            UIView.animate(withDuration: 0.25) {
+//                if self.sectionIsExpanded {
+//                    self.btn.transform = CGAffineTransform.identity
+//                } else {
+//                    self.btn.transform = CGAffineTransform(rotationAngle: -CGFloat.pi)
+//                }
+//            }
+//        }
+//    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
